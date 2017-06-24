8a2844e71907b9650fc26edf82b0d9ad1d5da68b
Javi Palacios <javi@fjp.es>
2017-06-25 01:11:53 +0200

    New exercise: Fibonacci Sequence

---
+++ Python/fibonacci.py
@@ -0,0 +1,28 @@
+# A simple code to practice recursion using the Fibonacci Sequence
+
+def fibonacci(number):
+   if number <= 1:
+       return 1
+   else:
+       return(fibonacci(number-1) + fibonacci(number-2))
+
+while True:
+    try:
+        numberOfTerms = int(input("Enter how many terms of the Fibonacci " +
+        "Sequence you want to find: "))
+    except ValueError:
+        print("Please, enter a positive integer greater than 0")
+        continue
+    else:
+        while numberOfTerms <= 0:
+            print("Please, enter a positive integer greater than 0")
+            try:
+                numberOfTerms = int(input("Enter how many terms of the " +
+                "Fibonacci Sequence you want to find: "))
+            except ValueError:
+                continue
+        print("The first {} terms of the Fibonacci Sequence are:" \
+        .format(numberOfTerms))
+        for i in range(numberOfTerms):
+            print(fibonacci(i), end=" ")
+        break
