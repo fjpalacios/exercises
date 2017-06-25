06eca9689a3489d9d0dba0b68548ffc447754cb4
Javi Palacios <javi@fjp.es>
2017-06-25 10:39:03 +0200

    Adding an option to continue w/ program at the end

--- Python/fibonacci.py
+++ Python/fibonacci.py
@@ -6,23 +6,37 @@ def fibonacci(number):
     else:
         return(fibonacci(number-1) + fibonacci(number-2))
 
-while True:
-    try:
-        numberOfTerms = int(input("Enter how many terms of the Fibonacci " +
-        "Sequence you want to find: "))
-    except ValueError:
-        print("Please, enter a positive integer greater than 0")
-        continue
+def continueProgram():
+    exit = input("\nDo you want to continue with the program? [Y/n] ")
+    if exit == "y" or exit == "":
+        main()
+    elif exit == "n":
+        raise SystemExit
     else:
-        while numberOfTerms <= 0:
+        print("You entered {}, but it is a wrong command. Good bye!". format(exit))
+        raise SystemExit
+
+def main():
+    while True:
+        try:
+            numberOfTerms = int(input("Enter how many terms of the Fibonacci " +
+            "Sequence you want to find: "))
+        except ValueError:
             print("Please, enter a positive integer greater than 0")
-            try:
-                numberOfTerms = int(input("Enter how many terms of the " +
-                "Fibonacci Sequence you want to find: "))
-            except ValueError:
-                continue
-        print("The first {} terms of the Fibonacci Sequence are:" \
-        .format(numberOfTerms))
-        for i in range(numberOfTerms):
-            print(fibonacci(i), end=" ")
-        break
+            continue
+        else:
+            while numberOfTerms <= 0:
+                print("Please, enter a positive integer greater than 0")
+                try:
+                    numberOfTerms = int(input("Enter how many terms of the " +
+                    "Fibonacci Sequence you want to find: "))
+                except ValueError:
+                    continue
+            print("The first {} terms of the Fibonacci Sequence are:" \
+            .format(numberOfTerms))
+            for i in range(numberOfTerms):
+                print(fibonacci(i), end=" ")
+            continueProgram()
+
+if __name__ == "__main__":
+    main()
