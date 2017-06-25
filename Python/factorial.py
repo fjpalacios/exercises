ffdac7149e3ad255c6e56b56cecc4f9a2aad42bb
Javi Palacios <javi@fjp.es>
2017-06-25 10:40:10 +0200

    New exercise: Factorial of a number (n!)

---
+++ Python/factorial.py
@@ -0,0 +1,39 @@
+# A simple code to practice recursion using the Factorial of a number (n!)
+
+def factorial(number):
+    factorial = 1
+    while number > 0:
+        factorial = factorial * number
+        number -= 1
+    return factorial
+
+def continueProgram():
+    exit = input("Do you want to continue with the program? [Y/n] ")
+    if exit == "y" or exit == "":
+        main()
+    elif exit == "n":
+        raise SystemExit
+    else:
+        print("You entered {}, but it is a wrong command. Good bye!". format(exit))
+        raise SystemExit
+
+def main():
+    while True:
+        try:
+            numberOfTerms = int(input("Enter a number: "))
+        except ValueError:
+            print("Please, enter a positive integer")
+            continue
+        else:
+            while numberOfTerms < 0:
+                print("Please, enter a positive integer")
+                try:
+                    numberOfTerms = int(input("Enter a number: "))
+                except ValueError:
+                    continue
+            valueFactorial = factorial(numberOfTerms)
+            print("Factorial of", numberOfTerms, "is", valueFactorial)
+            continueProgram()
+
+if __name__ == "__main__":
+    main()
