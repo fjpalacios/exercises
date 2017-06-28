a1874885c1ec7a498db7876d3e7e1db7d74a3949
Javi Palacios <javi@fjp.es>
2017-06-28 23:45:46 +0200

    New exercise: degree two equation solver
    In addition to the well-known formula, there are two conditionals which use another way to solve this equation when it has no b or c terms.

---
+++ Python/quadraticformula.py
@@ -0,0 +1,76 @@
+'''A simple code to practice Control Structures solving an equation like
+ax^2+bx+c (degree two) using the quadratic formula.
+There are conditionals (if) which use another way to solve it
+when the equation is incomplete (it has no b or c terms)'''
+
+
+from math import sqrt
+from decimal import Decimal
+
+
+def main():
+    termA = setSide("a")
+    termB = setSide("b", termA)
+    termC = setSide("c", termA, termB)
+    quadraticFormula(termA, termB, termC)
+    continueProgram()
+
+
+def setSide(term, termA=None, termB=None):
+    while True:
+        if term == "a" or termB == "x":
+            termValue = input("Enter value for {}: ".format(term))
+        else:
+            termValue = input("Enter value for {0} (if this equation has "
+                              "no term {0} enter x): "
+                              .format(term))
+        if termValue != "x":
+            try:
+                return float(termValue)
+                break
+            except ValueError:
+                print("Please, enter a proper value")
+        elif termValue == "x" and term != "a" and termB != "x":
+            return termValue
+            break
+        else:
+            print("Please, enter a proper value")
+
+
+def quadraticFormula(termA, termB, termC):
+    if termB == "x":
+        if termA < 0:
+            termA = -(termA)
+        elif termC < 0:
+            termC = -(termC)
+        solutionOne = sqrt(termC/termA)
+        solutionTwo = -(solutionOne)
+        print("Solution one: {}".format(Decimal(solutionOne).normalize()))
+        print("Solution two: {}".format(Decimal(solutionTwo).normalize()))
+    elif termC == "x":
+        solutionOne = 0
+        solutionTwo = -(termB)/termA
+        print("Solution one: {}".format(Decimal(solutionOne).normalize()))
+        print("Solution two: {}".format(Decimal(solutionTwo).normalize()))
+    else:
+        print("{}, {} y {}".format(termA, termB, termC))
+        solutionOne = (-(termB)+sqrt(termB**2-4*termA*termC))/(2*termA)
+        solutionTwo = (-(termB)-sqrt(termB**2-4*termA*termC))/(2*termA)
+        print("Solution one: {}".format(Decimal(solutionOne).normalize()))
+        print("Solution two: {}".format(Decimal(solutionTwo).normalize()))
+
+
+def continueProgram():
+    exit = input("Do you want to continue with the program? [Y/n] ")
+    if exit == "y" or exit == "":
+        main()
+    elif exit == "n":
+        raise SystemExit
+    else:
+        print("You entered {}, but it is a wrong command. Good bye!"
+              .format(exit))
+        raise SystemExit
+
+
+if __name__ == "__main__":
+    main()
