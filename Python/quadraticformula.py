b9921c6564ffdcf4c5634822981b4f8d8bda1fd6
Javi Palacios <javi@fjp.es>
2017-07-04 20:48:52 +0200

    Recoding the code from scratch
    This new improved version of the exercise now has a class (OOP paradigm) and all the peculiarities of this type of equations (two or a single root, unsolvable equations in real numbers, incomplete equations...) are contemplated

--- Python/quadraticformula.py
+++ Python/quadraticformula.py
@@ -1,4 +1,4 @@
-'''A simple code to practice Control Structures solving an equation like
+'''A simple code to practice OOP & Control Structures solving an equation like
 ax^2+bx+c (degree two) using the quadratic formula.
 There are conditionals (if) which use another way to solve it
 when the equation is incomplete (it has no b or c terms)'''
@@ -8,64 +8,77 @@ from math import sqrt
 from decimal import Decimal
 
 
-def main():
-    termA = setSide("a")
-    termB = setSide("b", termA)
-    termC = setSide("c", termA, termB)
-    quadraticFormula(termA, termB, termC)
-    continueProgram()
+class QuadraticFormula:
+    def __init__(self, termA, termB, termC):
+        self.termA = termA
+        self.termB = termB
+        self.termC = termC
+
+    def getDiscriminant(self):
+        return self.termB**2-4*self.termA*self.termC
+
+    def getRoot(self):
+        root = self.formula()
+        print("Solution: {}".format(Decimal(root[0]).normalize()))
+
+    def getRoots(self):
+        roots = self.formula()
+        print("Solution one: {}".format(Decimal(roots[0]).normalize()))
+        print("Solution two: {}".format(Decimal(roots[1]).normalize()))
+
+    def formula(self):
+        solutions = []
+        if self.termB == "x":
+            if self.termA < 0:
+                self.termA = -(self.termA)
+            elif self.termC < 0:
+                self.termC = -(self.termC)
+            solutionOne = sqrt(self.termC/self.termA)
+            solutionTwo = -(solutionOne)
+            solutions.append(solutionOne)
+            solutions.append(solutionTwo)
+        elif self.termC == "x":
+            solutionOne = 0
+            solutionTwo = -(self.termB)/self.termA
+            solutions.append(solutionOne)
+            solutions.append(solutionTwo)
+        else:
+            solutionOne = ((-self.termB+sqrt(self.getDiscriminant()))
+                           / (2*self.termA))
+            solutionTwo = ((-self.termB-sqrt(self.getDiscriminant()))
+                           / (2*self.termA))
+            solutions.append(solutionOne)
+            solutions.append(solutionTwo)
+        return solutions
+
+    def solve(self):
+        if self.getDiscriminant() > 0:
+            self.getRoots()
+        elif self.getDiscriminant() == 0:
+            self.getRoot()
+        else:
+            print("This equation has no real numbers solution")
 
 
 def setSide(term, termA=None, termB=None):
     while True:
-        if term == "a" or termB == "x":
+        if term == "a" or termB == 0:
             termValue = input("Enter value for {}: ".format(term))
         else:
             termValue = input("Enter value for {0} (if this equation has "
-                              "no term {0} enter x): "
+                              "no term {0} enter 0): "
                               .format(term))
-        if termValue != "x":
+        if termValue == "0" and term != "a" and termB != 0:
+            return 0
+            break
+        elif termValue != "0":
             try:
                 return float(termValue)
                 break
             except ValueError:
                 print("Please, enter a proper value")
-        elif termValue == "x" and term != "a" and termB != "x":
-            return termValue
-            break
         else:
             print("Please, enter a proper value")
-
-
-def getDiscriminant(termA, termB, termC):
-    return termB**2-4*termA*termC
-
-
-def quadraticFormula(termA, termB, termC):
-    if getDiscriminant(termA, termB, termC) >= 0:
-        if termB == "x":
-            if termA < 0:
-                termA = -(termA)
-            elif termC < 0:
-                termC = -(termC)
-            solutionOne = sqrt(termC/termA)
-            solutionTwo = -(solutionOne)
-            print("Solution one: {}".format(Decimal(solutionOne).normalize()))
-            print("Solution two: {}".format(Decimal(solutionTwo).normalize()))
-        elif termC == "x":
-            solutionOne = 0
-            solutionTwo = -(termB)/termA
-            print("Solution one: {}".format(Decimal(solutionOne).normalize()))
-            print("Solution two: {}".format(Decimal(solutionTwo).normalize()))
-        else:
-            solutionOne = ((-termB+sqrt(getDiscriminant(termA, termB, termC)))
-                           / (2*termA))
-            solutionTwo = ((-termB-sqrt(getDiscriminant(termA, termB, termC)))
-                           / (2*termA))
-            print("Solution one: {}".format(Decimal(solutionOne).normalize()))
-            print("Solution two: {}".format(Decimal(solutionTwo).normalize()))
-    else:
-        print("This equation has no real solutions")
 
 
 def continueProgram():
@@ -78,6 +91,15 @@ def continueProgram():
         print("You entered {}, but it is a wrong command. Good bye!"
               .format(exit))
         raise SystemExit
+
+
+def main():
+    termA = setSide("a")
+    termB = setSide("b", termA)
+    termC = setSide("c", termA, termB)
+    equation = QuadraticFormula(termA, termB, termC)
+    equation.solve()
+    continueProgram()
 
 
 if __name__ == "__main__":
