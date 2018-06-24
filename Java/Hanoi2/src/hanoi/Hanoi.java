704604caeb4d81544b4302cd2b774a4a75112f13
Javi Palacios <javi@fjp.es>
2018-06-24 20:27:58 +0200

    Improving my Java's version Hanoi code

--- Java/Hanoi2/src/hanoi/Hanoi.java
+++ Java/Hanoi2/src/hanoi/Hanoi.java
@@ -1,7 +1,12 @@
 // A simple code to practice recursion solving the game of the Tower of Hanoi
+package hanoi;
 
 import java.util.Scanner;
 
+/**
+ *
+ * @author Javi Palacios <javi@fjp.es>
+ */
 public class Hanoi {
 
     private static boolean continueProgram = true;
@@ -9,31 +14,25 @@ public class Hanoi {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         while (continueProgram) {
-            int numberOfDisks = intGreaterThan0("Enter a number of disks: ", scan);
-            int numberOfMoves = (int) (Math.pow(2, numberOfDisks) - 1);
-            System.out.printf("At least %d moves required to solve this game %n",
-                    numberOfMoves);
-            hanoi(numberOfDisks, 1, 2, 3);
-            continueProgram(scan);
+            try {
+                int numberOfDisks = intGreaterThan0("Enter a number of disks: ", scan);
+                int numberOfMoves = (int) (Math.pow(2, numberOfDisks) - 1);
+                System.out.printf("At least %d moves required to solve this game %n",
+                        numberOfMoves);
+                hanoi(numberOfDisks, 1, 2, 3);
+                continueProgram(scan);
+            } catch (NumberFormatException ex) {
+                System.out.println("Please, enter a positive integer.");
+            }
         }
     }
 
-    private static int intGreaterThan0(String message, Scanner scan) {
+    private static int intGreaterThan0(String message, Scanner scan)
+            throws NumberFormatException {
         int intGreaterThan0 = 0;
-        boolean integer = false;
         do {
-            try {
-                System.out.print(message);
-                intGreaterThan0 = Integer.parseInt(scan.nextLine());
-                integer = true;
-            } catch (Exception e) {
-                System.out.println("Please enter a positive integer.");
-            }
-            if (integer && intGreaterThan0 < 1) {
-                System.out.println("Please enter a positive integer "
-                        + "greater than 0.");
-                integer = false;
-            }
+            System.out.print(message);
+            intGreaterThan0 = Integer.parseInt(scan.nextLine());
         } while (intGreaterThan0 < 1);
         return intGreaterThan0;
     }
@@ -43,19 +42,19 @@ public class Hanoi {
             System.out.printf("Moving disk %d from stack %d to stack %d%n",
                     numberOfDisks, initialStack, finalStack);
         } else {
-            hanoi(numberOfDisks-1, initialStack, finalStack, auxiliarStack);
+            hanoi(numberOfDisks - 1, initialStack, finalStack, auxiliarStack);
             System.out.printf("Moving disk %d from stack %d to stack %d%n",
                     numberOfDisks, initialStack, finalStack);
-            hanoi(numberOfDisks-1, auxiliarStack, initialStack, finalStack);
+            hanoi(numberOfDisks - 1, auxiliarStack, initialStack, finalStack);
         }
     }
 
     private static void continueProgram(Scanner scan) {
         System.out.print("\nDo you want to continue with the program? [Y/n] ");
         String exit = scan.nextLine();
-        if ("y".equals(exit) || "".equals(exit)) {
+        if ("y".equalsIgnoreCase(exit) || "".equals(exit)) {
             continueProgram = true;
-        } else if ("n".equals(exit)) {
+        } else if ("n".equalsIgnoreCase(exit)) {
             continueProgram = false;
         } else {
             System.out.println("You entered a wrong command. Good bye!");
