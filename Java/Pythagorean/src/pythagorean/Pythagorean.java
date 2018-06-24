b8b4c374d159fc00e7a8f44e8bd84f2e984d755a
Javi Palacios <javi@fjp.es>
2018-06-25 01:30:02 +0200

    Improving my Java's version Pythagorean code

--- Java/Pythagorean/src/pythagorean/Pythagorean.java
+++ Java/Pythagorean/src/pythagorean/Pythagorean.java
@@ -1,10 +1,14 @@
 /* A simple code to practice Control Structures solving the missing side
  * of a right triangle using the Pythagorean theorem
  */
+package pythagorean;
 
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
+/**
+ *
+ * @author Javi Palacios <javi@fjp.es>
+ */
 public class Pythagorean {
 
     private static boolean continueProgram = true;
@@ -21,61 +25,47 @@ public class Pythagorean {
     }
 
     private static float setSide(Scanner scan) {
-        return numberGreaterThan0("Enter leg one (" +
-                "if it is the missing side enter 0): ", true, scan);
+        return getPossitiveNumberOrZero("Enter leg one ("
+                + "if it is the missing side enter 0): ", true, scan);
     }
 
     private static float setSide(float legOne, Scanner scan) {
         if (legOne == 0) {
-            return numberGreaterThan0("Enter leg two: ", false, scan);
-        } else {
-            return numberGreaterThan0("Enter leg two (" +
-                    "if it is the missing side enter 0): ", true, scan);
+            return getPossitiveNumberOrZero("Enter leg two: ", false, scan);
         }
+        return getPossitiveNumberOrZero("Enter leg two ("
+                + "if it is the missing side enter 0): ", true, scan);
     }
 
     private static float setSide(float legOne, float legTwo, Scanner scan) {
         if (legOne == 0 || legTwo == 0) {
-            return numberGreaterThan0("Enter hypotenuse: ", false, scan);
-        } else {
-            return 0;
+            return getPossitiveNumberOrZero("Enter hypotenuse: ", false, scan);
         }
+        return 0;
     }
 
-    private static float numberGreaterThan0(String message, boolean missing, Scanner scan) {
-        float number = 0;
-        boolean scanNumber = false;
-        while (!scanNumber) {
-            do {
-                try {
-                    System.out.print(message);
-                    number = Float.parseFloat(scan.nextLine());
-                    if (missing == false && number == 0) {
-                        throw new Exception();
-                    } else {
-                        scanNumber = true;
-                    }
-                } catch (Exception e) {
-                    System.out.println("Please enter a positive number.");
-                }
-                if (scanNumber && number < 0) {
-                    System.out.println("Please enter a positive number.");
-                    scanNumber = false;
-                }
-            } while (number < 0);
-        }
+    private static float getPossitiveNumberOrZero(String message,
+            boolean canBeZero, Scanner scan) {
+        float number = -1;
+        do {
+            try {
+                System.out.print(message);
+                number = Float.parseFloat(scan.nextLine());
+            } catch (NumberFormatException ex) {
+                System.out.println("Please, enter only numbers.");
+            }
+        } while ((canBeZero && number == -1) || (!canBeZero && number < 1));
         return number;
     }
 
     private static void missingSide(float legOne, float legTwo, float hypotenuse) {
         double missingSide = pythagorean(legOne, legTwo, hypotenuse);
-        DecimalFormat decimal = new DecimalFormat("0.###");
         if (legOne == 0) {
-            System.out.printf("Leg one is: %s", decimal.format(missingSide));
+            System.out.printf(String.format("Leg one is: %.3f", missingSide));
         } else if (legTwo == 0) {
-            System.out.printf("Leg two is: %s", decimal.format(missingSide));
+            System.out.printf(String.format("Leg two is: %.3f", missingSide));
         } else {
-            System.out.printf("Hypotenuse is: %s", decimal.format(missingSide));
+            System.out.printf(String.format("Hypotenuse is: %.3f", missingSide));
         }
     }
 
@@ -92,9 +82,9 @@ public class Pythagorean {
     private static void continueProgram(Scanner scan) {
         System.out.print("\nDo you want to continue with the program? [Y/n] ");
         String exit = scan.nextLine();
-        if ("y".equals(exit) || "".equals(exit)) {
+        if ("y".equalsIgnoreCase(exit) || "".equals(exit)) {
             continueProgram = true;
-        } else if ("n".equals(exit)) {
+        } else if ("n".equalsIgnoreCase(exit)) {
             continueProgram = false;
         } else {
             System.out.println("You entered a wrong command. Good bye!");
--- Java/Pythagorean/test/PythagoreanTest.java
+++ Java/Pythagorean/test/PythagoreanTest.java
@@ -1,3 +1,4 @@
+import pythagorean.Pythagorean;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
