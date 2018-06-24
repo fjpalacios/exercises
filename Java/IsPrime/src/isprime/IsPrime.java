fcca2a2acb4c8b668ae55fb302c51c3b1eb80e6b
Javi Palacios <javi@fjp.es>
2018-06-24 20:49:26 +0200

    Improving my Java's version isPrime code

--- Java/IsPrime/src/isprime/IsPrime.java
+++ Java/IsPrime/src/isprime/IsPrime.java
@@ -1,13 +1,17 @@
 /*
 * A simple code to practice finding out if a number is prime or not
 * If it is not a prime number, its prime factorization is printed
-*/
+ */
+package isprime;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
+/**
+ *
+ * @author Javi Palacios <javi@fjp.es>
+ */
 public class IsPrime {
 
     private static boolean continueProgram = true;
@@ -15,39 +19,30 @@ public class IsPrime {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         while (continueProgram) {
-            int number = intGreaterThan0("Enter the number that you" +
-                    " want to know if is a prime number: ", scan);
-            boolean isPrimeNumber = isPrime(number);
-            if (isPrimeNumber) {
-                System.out.printf("%d is a prime number", number);
-            } else {
-                List<Integer> intList = primeFactors(number);
-                String factorization = intList.stream()
-                        .map(Object::toString)
-                        .collect(Collectors.joining("*"));
-                System.out.printf("%d is not a prime number (%s)", number,
-                        factorization);
+            try {
+                int number = intGreaterThan0("Enter the number that you"
+                        + " want to know if is a prime number: ", scan);
+                if (isPrime(number)) {
+                    System.out.printf("%d is a prime number", number);
+                } else {
+                    List<Integer> intList = primeFactors(number);
+                    String factorization = factorization(intList);
+                    System.out.printf("%d is not a prime number (%s)", number,
+                            factorization);
+                }
+                continueProgram(scan);
+            } catch (NumberFormatException ex) {
+                System.out.println("Please, enter a positive integer.");
             }
-            continueProgram(scan);
         }
     }
 
-    private static int intGreaterThan0(String message, Scanner scan) {
+    private static int intGreaterThan0(String message, Scanner scan)
+            throws NumberFormatException {
         int intGreaterThan0 = 0;
-        boolean integer = false;
         do {
-            try {
-                System.out.print(message);
-                intGreaterThan0 = Integer.parseInt(scan.nextLine());
-                integer = true;
-            } catch (Exception e) {
-                System.out.println("Please enter a positive integer.");
-            }
-            if (integer && intGreaterThan0 < 1) {
-                System.out.println("Please enter a positive integer "
-                        + "greater than 0.");
-                integer = false;
-            }
+            System.out.print(message);
+            intGreaterThan0 = Integer.parseInt(scan.nextLine());
         } while (intGreaterThan0 < 1);
         return intGreaterThan0;
     }
@@ -72,12 +67,20 @@ public class IsPrime {
         return primeFactors;
     }
 
+    public static String factorization(List<Integer> list) {
+        String string = "";
+        for (Integer integer : list) {
+            string += String.format("%d*", integer);
+        }
+        return string.substring(0, string.length() - 1);
+    }
+
     private static void continueProgram(Scanner scan) {
         System.out.print("\nDo you want to continue with the program? [Y/n] ");
         String exit = scan.nextLine();
-        if ("y".equals(exit) || "".equals(exit)) {
+        if ("y".equalsIgnoreCase(exit) || "".equals(exit)) {
             continueProgram = true;
-        } else if ("n".equals(exit)) {
+        } else if ("n".equalsIgnoreCase(exit)) {
             continueProgram = false;
         } else {
             System.out.println("You entered a wrong command. Good bye!");
