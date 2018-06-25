6900a2a2863f93f9a331cd993756f3c9c7332208
Javi Palacios <javi@fjp.es>
2018-06-25 16:43:40 +0200

    Improving my Python's version of Collatz code

--- Python/collatz.py
+++ Python/collatz.py
@@ -32,28 +32,23 @@ def int_greather_than_0(message):
     while True:
         try:
             _int_greather_than_0 = int(input(message))
+            if _int_greather_than_0 > 0:
+                break
         except ValueError:
             print("Please, enter a positive integer greater than 0")
-            continue
-        else:
-            while _int_greather_than_0 <= 0:
-                print("Please, enter a positive integer greater than 0")
-                try:
-                    _int_greather_than_0 = int(input(message))
-                except ValueError:
-                    continue
-            return _int_greather_than_0
+    return _int_greather_than_0
 
 
 def continue_program():
     system_exit = input("Do you want to continue with the program? [Y/n] ")
+    system_exit = system_exit.lower()
     if system_exit == "y" or system_exit == "":
         main()
     elif system_exit == "n":
         raise SystemExit
     else:
         print("You entered {}, but it is a wrong command. Good bye!"
-              .format(exit))
+              .format(system_exit))
         raise SystemExit
 
 
